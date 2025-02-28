821307db1cf7e055e0526afa8199562818c19e93
Bo Shu <shu00003@algonquinlive.com>
2025-02-27 22:03:46 -0500

    impl: basic calculation

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,140 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+
+function Xian({lrgToast, miniToast}) {
+  const XIAN = {
+    butter: 150,
+    salt: 1,
+    sugar_powder: 78,
+    egg_white: 56,
+    milk_powder: 160,
+    black_tea_powder: 6,
+    caramel_cookie_crumbles: 56
+  }
+
+  const ONELRGXIAN = 90;
+  const ONEMINIXIAN = 40;
+
+  const calXianRatio = () => {
+    const xianWeight = Object.values(XIAN).reduce((acc, cur) => acc + cur, 0);
+    const ratio = (lrgToast * ONELRGXIAN + miniToast * ONEMINIXIAN)/xianWeight;
+    return ratio;
+  }
+  return (
+    <ul>
+      <li>黄油： {Math.ceil(XIAN.butter*calXianRatio())}g</li>
+      <li>盐: {Math.ceil(XIAN.salt*calXianRatio())}g</li>
+      <li>糖粉: {Math.ceil(XIAN.sugar_powder*calXianRatio())}g</li>
+      <li>蛋液: {Math.ceil(XIAN.egg_white*calXianRatio())}g</li>
+      <li>奶粉: {Math.ceil(XIAN.milk_powder*calXianRatio())}g</li>
+      <li>红茶粉: {Math.ceil(XIAN.black_tea_powder*calXianRatio())}g</li>
+      <li>焦糖饼干碎: {Math.ceil(XIAN.caramel_cookie_crumbles*calXianRatio())}g</li>
+    </ul>
+  );
+}
+
+function Mian({lrgToast, miniToast}) {
+  
+  const MIANTUAN = {
+    flour: 1400,
+    poolish: 700,
+    yeast: 14,
+    milk_powder: 70,
+    sugar: 280,
+    water: 403,
+    milk: 280,
+    egg: 175,
+    salt: 26,
+    butter: 105
+  }
+
+  const YEZHONG = {
+    flour: {
+      high_gluten: 210,
+      low_gluten: 140
+    },
+    water: 350,
+    yeast: 4,
+    honey: 7
+  }
+
+  const ONELRGMIAN = 250;
+  const ONEMINIMIAN = 110;
+
+  const calMianRatio = () => {
+    const mianWeight = Object.values(MIANTUAN).reduce((acc, cur) => acc + cur, 0);
+    const ratio = (lrgToast * ONELRGMIAN + miniToast * ONEMINIMIAN)/mianWeight;
+    return ratio;
+  }
+
+  return (
+    <div className="flex">
+      <ul>
+        <li>粉： {Math.ceil(MIANTUAN.flour*calMianRatio())}g</li>
+        <li>液种: {Math.ceil(MIANTUAN.poolish*calMianRatio())}g</li>
+        <li>酵母: {Math.ceil(MIANTUAN.yeast*calMianRatio())}g</li>
+        <li>奶粉: {Math.ceil(MIANTUAN.milk_powder*calMianRatio())}g</li>
+        <li>糖: {Math.ceil(MIANTUAN.sugar*calMianRatio())}g</li>
+        <li>水: {Math.ceil(MIANTUAN.water*calMianRatio())}g</li>
+        <li>牛奶: {Math.ceil(MIANTUAN.milk*calMianRatio())}g</li>
+        <li>鸡蛋: {Math.ceil(MIANTUAN.egg*calMianRatio())}g</li>
+        <li>盐: {Math.ceil(MIANTUAN.salt*calMianRatio())}g</li>
+        <li>黄油: {Math.ceil(MIANTUAN.butter*calMianRatio())}g</li>
+      </ul>
+    
+      <ul>
+        <li className="flex">
+          粉: 
+          <ul>
+            <li>高 {Math.ceil(YEZHONG.flour.high_gluten*calMianRatio())}g</li>
+            <li>低 {Math.ceil(YEZHONG.flour.low_gluten*calMianRatio())}g</li>
+          </ul>
+        </li>
+        <li>水: {Math.ceil(YEZHONG.water*calMianRatio())}g</li>
+        <li>酵母: {Math.ceil(YEZHONG.yeast*calMianRatio())}g</li>
+        <li>蜂蜜: {Math.ceil(YEZHONG.honey*calMianRatio())}g</li>
+      </ul>
+    </div>
+  );
+}
+
+function Toast() {
+  const [lrgToast, setLrgToast] = useState(0);
+  const [miniToast, setMiniToast] = useState(0);
+
+  return (
+    <>
+    <label htmlFor="largeNum">Number of large size you would like to make:</label>
+    <input id="largeNum" type="number" value={lrgToast} onChange={(e)=>setLrgToast(e.target.valueAsNumber)}/>
+    <label htmlFor="miniNum">Number of mini size you would like to make:</label>
+    <input id="miniNum" type="number" value={miniToast} onChange={(e)=>setMiniToast(e.target.valueAsNumber)}/>
+
+    <div style={{display: "flex"}}>
+      <div className="container sm">
+        <Mian lrgToast={lrgToast} miniToast={miniToast} />
+      </div>
+      <div className="container sm">
+        <Xian lrgToast={lrgToast} miniToast={miniToast} />
+      </div>
+    </div>
+    </>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <h1 className="text-blue-500">Dumi Bakery</h1>
+      <Toast />
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
