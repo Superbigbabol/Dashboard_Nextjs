ef328602e85538cb192a76db4960162e9cc528db
Bo Shu <shu00003@algonquinlive.com>
2025-03-02 15:26:17 -0500

    finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 'use client'
 import { useState } from "react";
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import Image from 'next/image';
+import { lusitana } from "@/app/ui/fonts";
 
 type TQProps = {
   lrgToast: number;
@@ -111,10 +116,10 @@ function Toast() {
 
   return (
     <>
-    <label htmlFor="largeNum">Number of large size you would like to make:</label>
-    <input id="largeNum" type="number" value={lrgToast} onChange={(e)=>setLrgToast(e.target.valueAsNumber)}/>
-    <label htmlFor="miniNum">Number of mini size you would like to make:</label>
-    <input id="miniNum" type="number" value={miniToast} onChange={(e)=>setMiniToast(e.target.valueAsNumber)}/>
+    <label htmlFor="largeNum">Number of large size:</label>
+    <input className="w-40" id="largeNum" type="number" min="0" value={lrgToast} onChange={(e)=>setLrgToast(e.target.valueAsNumber)}/>
+    <label htmlFor="miniNum">Number of mini size:</label>
+    <input className="w-40" id="miniNum" type="number" value={miniToast} onChange={(e)=>setMiniToast(e.target.valueAsNumber)}/>
 
     <div style={{display: "flex"}}>
       <div className="container sm">
@@ -128,20 +133,47 @@ function Toast() {
   );
 }
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <h1 className="text-blue-500">Dumi Bakery</h1>
-      <Toast />
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
       </div>
-    </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <a href="https://nextjs.org/learn/" className="text-blue-500">
+              Next.js Learn Course
+            </a>
+            , brought to you by Vercel.
+          </p>
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
+        </div>
+      </div>
+    </main>
   );
 }
